acb6e97befd9a733102fd9a2c5c7452f4ac4f43c
jonaniano <jonaniano@Jelenas-MacBook-Pro.local>
2014-08-18 01:09:36 -0700

    asfd

--- public/scripts/softphone.js
+++ public/scripts/softphone.js
@@ -326,6 +326,8 @@ $(function() {
       sforce.interaction.cti.onClickToDial(startCall);
       desk.interaction.cti.enableClickToDial();
       desk.interaction.cti.onClickToDial(startCall);
+      desk.interaction.cti.setSoftphoneHeight(600);
+      desk.interaction.cti.setSoftphoneWidth(300);
       SP.functions.ready();
     });
 
